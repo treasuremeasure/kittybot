2396f2dc0aaba9fe4e89eb42a46b64f5684c9db3
Riza <orduhaniriza@gmail.com>
2024-10-23 12:44:27 +0300

    Клавиатура & кнопки

--- send_random_image.py
+++ send_random_image.py
@@ -1,5 +1,5 @@
 import requests
-from telebot import TeleBot
+from telebot import TeleBot, types
 
 
 bot = TeleBot(token='7688992831:AAFB6AHFFH42-WWA9YXVpuBGz4T3GaP07UY')
@@ -11,13 +11,23 @@ def send_random_cat():
     photo = response[0].get('url')
     return photo
 
-@bot.message_handler(commands=['newcat'])
+@bot.message_handler(commands=['start'])
+def wake_up(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_newcat = types.KeyboardButton('Новая кошка')
+    keyboard.row(
+        button_newcat, types.KeyboardButton('Новая собака')
+    )
+    keyboard.row(
+        types.KeyboardButton('Новый лев')
+    )
+    bot.send_message(message.chat.id, text=f'Привет, {message.from_user.first_name} посмотри какого котенка я тебе нашел', reply_markup=keyboard)
+    bot.send_photo(message.chat.id, send_random_cat())
+
+@bot.message_handler(func=lambda message: message.text == 'Новая кошка')
 def new_cat(message):
     bot.send_photo(message.chat.id, send_random_cat())
 
-@bot.message_handler(commands=['start'])
-def wake_up(message):
-    bot.send_photo(message.chat.id, send_random_cat())
-    bot.send_message(message.chat.id, text=f'Привет, {message.from_user.first_name} посмотри какого котенка я тебе нашел')
+
 
 bot.polling()
